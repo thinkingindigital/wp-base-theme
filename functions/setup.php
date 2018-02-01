27ef230f6c5cb0e03ce2a8e84f0bb984a98f1065
Andrew Kissel <andrew@thinkingindigital.com>
2018-02-01 13:26:36 -0600

    Admin Menu Bar custom menu

--- functions/setup.php
+++ functions/setup.php
@@ -147,3 +147,41 @@ if( function_exists('acf_add_options_page') ) {
 	));
     */
 }
+
+
+/**
+* Add Admin Menu Bar Items
+*/
+function add_toolbar_items($admin_bar){
+    $admin_bar->add_menu( array(
+        'id'    => 'my-item',
+        'title' => 'My Item',
+        'href'  => '#',
+        'meta'  => array(
+            'title' => __('My Item'),
+        ),
+    ));
+    $admin_bar->add_menu( array(
+        'id'    => 'my-sub-item',
+        'parent' => 'my-item',
+        'title' => 'My Sub Menu Item',
+        'href'  => '#',
+        'meta'  => array(
+            'title' => __('My Sub Menu Item'),
+            'target' => '_blank',
+            'class' => 'my_menu_item_class'
+        ),
+    ));
+    $admin_bar->add_menu( array(
+        'id'    => 'my-second-sub-item',
+        'parent' => 'my-item',
+        'title' => 'My Second Sub Menu Item',
+        'href'  => '#',
+        'meta'  => array(
+            'title' => __('My Second Sub Menu Item'),
+            'target' => '_blank',
+            'class' => 'my_menu_item_class'
+        ),
+    ));
+}
+//add_action('admin_bar_menu', 'add_toolbar_items', 100);
